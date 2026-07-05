c89bb06a1a6b9999fa69fd8d6ba54e2c4a2f1f14
Shrey <shreysingh11733@gmail.com>
2026-07-05 14:00:20 +0530

    _04_Responsive_Card

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,5 +1,6 @@
+import CardField from "../components/Card";
 import Loader from "../components/Lodder";
-import SearBar from "../components/searchBar";
+import SearchBar from "../components/searchBar";
 import { MOVIE_API_KEY } from "../config/config"
 import { useEffect, useState } from "react";
 
@@ -12,10 +13,21 @@ interface Movie {
   Type: string;
 }
 
+const movie = 
+  {
+    "Title": "Batman Begins",
+    "Year": "2005",
+    "imdbID": "tt0372784",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BMzA2NDQzZDEtNDU5Ni00YTlkLTg2OWEtYmQwM2Y1YTBjMjFjXkEyXkFqcGc@._V1_QL75_UX380_CR0,0,380,562_.jpg"
+}
+
+
 export default function Home() {
   const [movies, setMovies] = useState<Movie[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
 
 
 
@@ -23,26 +35,25 @@ export default function Home() {
     const searchMovie = async (title: string) => {
       setLoading(true);
       setError("");
-      
+
       try {
         const response = await fetch(`${MOVIE_API_KEY}&s=${title}`);
         const data = await response.json();
 
-        if(data.response === "True"){
-          searchMovie(data.Search);
-        }else{
-          searchMovie("");
-          setError(data.error)
+        if (data.Response === "True") {
+          setMovies(data.Search);
+        } else {
+          setMovies([]);
+          setError(data.Error);
         }
 
         console.log(data.Search);
-      } catch (e) {
+      } catch (error) {
         setError("Something went wrong");
       } finally {
         setLoading(false);
       }
-        
-      }
+    };
     
     
       useEffect(() => {
@@ -50,14 +61,30 @@ export default function Home() {
       }, [])
     return (
       <>
-        <SearBar />
-        {loading && <Loader />}
-        {!loading && error && (
-          <h3>{error}</h3>
-        )};
-        {!loading && !error && (
-          // <MovieList movies={movies} />
-          <h1>hello</h1>
+        <SearchBar />
+        {loading ? (
+          <Loader />
+        ) : (
+          <div
+            style={{
+              display: "flex",
+              flexWrap: "wrap",
+              gap: "20px",
+              justifyContent: "center",
+              marginTop: "30px",
+            }}
+          >
+            {movies.map((movie) => (
+              <CardField
+                key={movie.imdbID}
+                Title={movie.Title}
+                Year={movie.Year}
+                imdbID={movie.imdbID}
+                Type={movie.Type}
+                Poster={movie.Poster}
+              />
+            ))}
+          </div>
         )}
       </>
     );
