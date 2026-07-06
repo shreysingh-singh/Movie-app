5cc10abd0b61bee61303c5bc4f990c112eec4e97
Shrey <shreysingh11733@gmail.com>
2026-07-06 12:37:44 +0530

    _07_Added_backgournd

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,26 @@ import { useEffect } from 'react'
 import './App.css'
 import { MOVIE_API_KEY } from './config/config';
 import Home from './Pages/Home';
+import background from "../src/assets/Movie-app-bg.jpg"
 
 
 function App() {
 
   return (
     <>
-    <Home />
+      <div
+        style={{
+          backgroundImage: `url(${background})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          minHeight: "100vh",
+        }}
+      >
+        <Home />
+      </div>
     </>
-
-  )
+  );
 }
 
 export default App
